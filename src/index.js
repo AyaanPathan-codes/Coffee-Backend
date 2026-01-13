2ca38db17b2b92e45c84e099e78dc6b37eee340d
confirm <ayaanpathanra@gmail.com>
2026-01-13 15:45:53 +0530

    connect to  mongo atlas

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv'
+dotenv.config({
+    path:'./env'
+})
+import connectDB from "./db/index.js";
+
+console.log(process.env.MONGODB_URI);
+
+connectDB();
+
+
+
+// ;( async ()=>{
+//     try{
+//        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//     }
+//     catch(error){
+//         console.error(error);
+
+//     }
+// })()
