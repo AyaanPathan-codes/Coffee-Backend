7a2b76ec3f6f50ddcc6dd6594100f2714f79e4bf
confirm <ayaanpathanra@gmail.com>
2026-01-30 21:24:54 +0530

    add code in controller of user

--- src/controllers/userRegister.controller.js
+++ src/controllers/userRegister.controller.js
@@ -1,10 +1,22 @@
 import {asyncHandler} from "../utils/asyncHandler.js"
-
+import {ApiError} from "../utils/apiError.js"
 
 const registerUser = asyncHandler(async (req,res)=>{
-    res.status(200).json({
-        message: "OK"
-    })
+    //email,fullName,password
+    const {email,fullName,password,username} = req.body
+    console.log("email :", email),
+    console.log("passowrd :", password),
+    console.log("username :", username)
+
+    if (
+  !fullName?.trim() ||
+  !email?.trim() ||
+  !password?.trim() ||
+  !username?.trim()
+) {
+  throw new ApiError(400, "All fields are required");
+}
+
 })
 
 export default registerUser;
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,22 @@
 import {Router} from 'express';
 import registerUser from '../controllers/userRegister.controller.js';
+import upload from "../middleware/multer.middleware.js"
 
 const routes = Router();
 
-routes.route("/register").post(registerUser)
+routes.route("/register").post(
+
+    upload.fields([
+        {
+            name:"avatar",
+            maxCount : 1
+        },
+        {
+            name : "coverImage",
+            maxCount : 1
+        }
+    ]),
+    registerUser
+)
 
 export default routes;
